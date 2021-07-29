d47d8f5b5529cc47a6b8793f917f9aa3ffbc3e09
Pedro Porras <pporras@pporras-usb2eth.xumak.gt>
2021-07-29 17:40:02 -0600

    Update of repository with new files with docker and redis

--- application.py
+++ application.py
@@ -1,10 +1,23 @@
 from flask import Flask
+import redis
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
+
+def get_hit_count():
+    retries = 5 
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 
 class Drink(db.Model):
